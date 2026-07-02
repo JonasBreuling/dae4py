d13cefd37cf13eaec8e91b74abcc80f81c8d4dc6
Jonas Breuling <jonas.breuling@inm.uni-stuttgart.de>
2026-07-02 09:49:10 +0200

    Corrected van der pol DAE function.

--- examples/index0/van_der_pol/van_der_pol.py
+++ examples/index0/van_der_pol/van_der_pol.py
@@ -1,10 +1,9 @@
 import numpy as np
 from dae4py.dae_problem import DAEProblem
 
-eps = 1e-3
+eps = 1e-2
 t0 = 0
-t1 = 5
-mu = 1 / np.sqrt(eps)
+t1 = 550
 
 
 def rhs(t, y):
@@ -12,7 +11,7 @@ def rhs(t, y):
     return np.array(
         [
             z2,
-            ((1 - z1**2) * z2 - z1) / eps,
+            (1 - z1**2) * z2 / eps - z1,
         ]
     )
 
@@ -21,7 +20,7 @@ def F(t, y, yp):
     # return yp - rhs(t, y)  # naive ODE call
     z1, z2 = y
     zp1, zp2 = yp
-    return np.array([zp1 - z2, eps * zp2 - (1 - z1**2) * z2 + z1])
+    return np.array([zp1 - z2, eps * zp2 - (1 - z1**2) * z2 + eps * z1])
 
 
 y0 = np.array([2, 0], dtype=float)
